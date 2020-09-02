e7d4b6a08635135e4f410a964cc72250f89b3c8f
Khalid Ahmed <55812528+khalidbahri@users.noreply.github.com>
2020-09-02 12:16:27 -0400

    Added one comment

--- AGMS/Program.cs
+++ AGMS/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+// made some changes..  
+
 namespace AGMS
 {
     public class Program
